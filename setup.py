649a9eef7f425a517a582b7615fc36b94dfd03ea
Munkhbayar Nergui <muunuu17@gmail.com>
2017-09-05 01:27:40 +0100

    expanded gitignore

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(name='SPARQLToSQL',
       description='An SPARQL-to-SQL translator',
       long_description='An SPARQL-to-SQL translator based on a semantics-preserving translation approach by Artem Chebotko',
       version='0.1.0',
-      url='https://github.com/munkhbayar17/SPARQLToSQL-to-sql',
+      url='https://github.com/munkhbayar17/sparql-to-sql',
       author='M. Nergui',
       author_email='muunuu17@gmail.com',
       license='Apache2',
@@ -18,6 +18,7 @@ setup(name='SPARQLToSQL',
       install_requires=[
           'PyYAML>=3.11'
       ],
+      python_requires='>=3.6',
       entry_points={
           'console_scripts': [
               'encrypt=SPARQLToSQL.main:translate'
